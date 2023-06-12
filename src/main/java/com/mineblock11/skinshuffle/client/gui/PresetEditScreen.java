64ef56b8b040694f7ada16d71c25fb980d8773c5
enjarai <enjarai123@gmail.com>
2023-06-12 22:06:04 +0200

    Neato

--- src/main/java/com/mineblock11/skinshuffle/client/gui/PresetEditScreen.java
+++ src/main/java/com/mineblock11/skinshuffle/client/gui/PresetEditScreen.java
@@ -22,6 +22,7 @@ import net.minecraft.util.math.MathHelper;
 import java.util.UUID;
 
 import static net.minecraft.client.gui.screen.world.CreateWorldScreen.FOOTER_SEPARATOR_TEXTURE;
+import static net.minecraft.client.gui.screen.world.CreateWorldScreen.LIGHT_DIRT_BACKGROUND_TEXTURE;
 
 public class PresetEditScreen extends SpruceScreen {
     private final Screen parent;
@@ -46,12 +47,14 @@ public class PresetEditScreen extends SpruceScreen {
 
     @Override
     protected void init() {
+        // Setup tabs, selecting the first one by default
         this.tabNavigation = TabNavigationWidget.builder(this.tabManager, this.width)
                 .tabs(new GeneralTab(), new SkinTab(), new GamePlayTab())
                 .build();
         this.addDrawableChild(this.tabNavigation);
         this.tabNavigation.selectTab(0, false);
 
+        // Setup grid layout for cancel and ok buttons
         this.grid = new GridWidget().setColumnSpacing(10);
         GridWidget.Adder adder = this.grid.createAdder(2);
         adder.add(ButtonWidget.builder(ScreenTexts.CANCEL, (button) -> {
@@ -87,12 +90,25 @@ public class PresetEditScreen extends SpruceScreen {
     public void render(DrawContext graphics, int mouseX, int mouseY, float delta) {
         super.render(graphics, mouseX, mouseY, delta);
 
+        int previewSpanX = 100 / 2;
+        int previewSpanY = 160 / 2;
+        int previewCenterX = this.width / 4;
+        int previewCenterY = this.height / 2;
+        graphics.drawBorder(previewCenterX - previewSpanX, previewCenterY - previewSpanY,
+                previewSpanX * 2, previewSpanY * 2, 0xDF000000);
+        graphics.fill(previewCenterX - previewSpanX + 1, previewCenterY - previewSpanY + 1,
+                previewCenterX + previewSpanX - 1, previewCenterY + previewSpanY - 1, 0x7F000000);
         GuiEntityRenderer.drawEntity(
-                graphics.getMatrices(), getX() + (this.width / 4), (int) (this.getY() + this.height / 1.6),
-                this.height / 4, getEntityRotation(), 0, 0, entity
+                graphics.getMatrices(), previewCenterX, previewCenterY + previewSpanY / 10 * 8, previewSpanY / 10 * 8,
+                getEntityRotation(), 0, 0, entity
         );
 
         graphics.drawTexture(FOOTER_SEPARATOR_TEXTURE, 0, MathHelper.roundUpToMultiple(this.height - 36 - 2, 2), 0.0F, 0.0F, this.width, 2, 32, 2);
+    }
+
+    @Override
+    public void renderBackgroundTexture(DrawContext context) {
+        context.drawTexture(LIGHT_DIRT_BACKGROUND_TEXTURE, 0, 0, 0, 0.0F, 0.0F, this.width, this.height, 32, 32);
     }
 
     private float getEntityRotation() {
