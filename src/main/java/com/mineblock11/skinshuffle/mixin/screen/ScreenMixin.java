df4f6a8988c0e5e9d0079148786fdb8a4967b180
Benonardo <75318636+Benonardo@users.noreply.github.com>
2023-07-05 15:51:00 +0200

    Use superclass mixin for close and onDisplayed hooks (#30)

---
+++ src/main/java/com/mineblock11/skinshuffle/mixin/screen/ScreenMixin.java
@@ -0,0 +1,50 @@
+/*
+ *
+ *     Copyright (C) 2023 Calum (mineblock11), enjarai
+ *
+ *     This library is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public
+ *     License as published by the Free Software Foundation; either
+ *     version 2.1 of the License, or (at your option) any later version.
+ *
+ *     This library is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this library; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *     USA
+ */
+
+package com.mineblock11.skinshuffle.mixin.screen;
+
+import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.Selectable;
+import net.minecraft.client.gui.screen.Screen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Screen.class)
+public abstract class ScreenMixin {
+
+    @Shadow protected abstract void remove(Element child);
+
+    @Shadow protected abstract <T extends Element & Drawable & Selectable> T addDrawableChild(T drawableElement);
+
+    @Inject(method = "close", at = @At("HEAD"))
+    protected void closeHook(CallbackInfo ci) {
+
+    }
+
+    @Inject(method = "onDisplayed", at = @At("HEAD"))
+    protected void onDisplayedHook(CallbackInfo ci) {
+
+    }
+
+}
