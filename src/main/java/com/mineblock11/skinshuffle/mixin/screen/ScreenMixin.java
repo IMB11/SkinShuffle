e9d8e11906bd7fa083d514c46bcc34d8109591b3
Benonardo <75318636+Benonardo@users.noreply.github.com>
2023-07-05 15:47:05 +0200

    Add one newline

--- src/main/java/com/mineblock11/skinshuffle/mixin/screen/ScreenMixin.java
+++ src/main/java/com/mineblock11/skinshuffle/mixin/screen/ScreenMixin.java
@@ -17,6 +17,7 @@
  *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
  *     USA
  */
+
 package com.mineblock11.skinshuffle.mixin.screen;
 
 import net.minecraft.client.gui.Drawable;
