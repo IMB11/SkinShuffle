68703e6d05eb5618d692eae58b8e0291087c0394
Calum <contact@mineblock11.dev>
2023-08-16 18:26:22 +0100

    Revert "fix: Try fix BiHashMap impl?"
    This reverts commit 6993b2195b1a0cb11dbfe0b6e2e87d4ecfb6a885.

--- src/main/java/com/mineblock11/skinshuffle/util/BiHashMap.java
+++ src/main/java/com/mineblock11/skinshuffle/util/BiHashMap.java
@@ -20,35 +20,60 @@
 
 package com.mineblock11.skinshuffle.util;
 
-import oshi.util.tuples.Pair;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class BiHashMap<K1, K2, V> {
-    private final HashMap<Pair<K1, K2>, V> internalMap = new HashMap<>();
+
+    private final Map<K1, Map<K2, V>> mMap;
+    private int sizeOfMap;
+
+    public BiHashMap() {
+        mMap = new HashMap<>();
+        sizeOfMap = 0;
+    }
 
     public void put(K1 key1, K2 key2, V value) {
-       internalMap.put(new Pair<>(key1, key2), value);
+        Map<K2, V> map;
+        if (mMap.containsKey(key1)) {
+            map = mMap.get(key1);
+        } else {
+            map = new HashMap<>();
+            mMap.put(key1, map);
+        }
+        map.put(key2, value);
+        sizeOfMap++;
     }
 
     public int getSize() {
-        return internalMap.size();
+        return sizeOfMap;
     }
 
     public Boolean containsKey(K1 key1) {
-        return internalMap.keySet().stream().anyMatch(pair -> pair.getA().equals(key1));
+        return mMap.containsKey(key1);
     }
 
+    public Boolean containsKey(K1 key1, K2 key2) {
+        Map<K2, V> map;
+        if (mMap.containsKey(key1)) {
+            map = mMap.get(key1);
+            return map.containsKey(key2);
+        } else
+            return false;
+    }
     public V get(K1 key1, K2 key2) {
-        return internalMap.get(new Pair<>(key1, key2));
+        if (mMap.containsKey(key1)) {
+            return mMap.get(key1).get(key2);
+        } else {
+            return null;
+        }
     }
     public boolean containsKeys(K1 key1, K2 key2) {
-        return internalMap.keySet().stream().anyMatch(pair -> pair.getA().equals(key1) && pair.getB().equals(key2));
+        return mMap.containsKey(key1) && mMap.get(key1).containsKey(key2);
     }
 
     public void clear() {
-        internalMap.clear();
+        mMap.clear();
     }
 
 }
