d26ba6cb0c8cb79288c5707f8143789d7ea025a5
Calum <contact@mineblock11.dev>
2024-08-22 16:00:16 +0100

    fix: Polytone issue

--- src/main/java/com/mineblock11/skinshuffle/client/gui/cursed/GuiEntityRenderer.java
+++ src/main/java/com/mineblock11/skinshuffle/client/gui/cursed/GuiEntityRenderer.java
@@ -26,10 +26,7 @@ import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.model.Dilation;
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.model.TexturedModelData;
-import net.minecraft.client.render.DiffuseLighting;
-import net.minecraft.client.render.OverlayTexture;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.*;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.MathHelper;
@@ -101,7 +98,7 @@ public class GuiEntityRenderer {
         model.render(
                 matrices,
                 vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(skin.getTexture())),
-                0,
+                LightmapTextureManager.MAX_LIGHT_COORDINATE,
                 OverlayTexture.DEFAULT_UV,
                 //? if >=1.21 {
                 0xFFFFFFFF
